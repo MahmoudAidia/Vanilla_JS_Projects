65835b09f73bd9ee0b90c3bb55e1a61953260734
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2023-11-03 14:10:21 +0200

    A New project: Landing Page with Menu slider and Modal

---
+++ 04-Movie_Seat/assests/script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".rows .seat.selected");
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    // console.log();
+    return [...seats].indexOf(seat);
+  });
+  console.log(seatsIndex);
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie Select Event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat select Event listner
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
